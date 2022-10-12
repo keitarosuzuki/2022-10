5e7cb7968c5129d1a7f71c2590a260f82a7dbf9d
keitarosuzuki <1548suzuki@gmail.com>
2022-10-12 18:13:23 +0900

    モジュール連携等

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -32,11 +32,38 @@ function createDB(thisId, thisName, thisPart){
 // };
 
 // allRead
-function allReadDB(){
+function allReadDB(theadTr, tbody){
     db.ref('member/').on('value', (snapshot) => {
         const data = snapshot.val();
         const ary = data.filter(x => data != null);
-        console.log(ary);
+        const objLength = ary.length;
+        const keyNames = Object.keys(ary[0]);
+        const keysLength = keyNames.length;
+
+        // thead ⇒ tr ⇒ th の数 → プロパティの数
+        for(let x = 0; x < keysLength; x++){
+            var theadThEl = document.createElement("th");
+            theadThEl.setAttribute("scope", "col");
+            theadThEl.textContent = keyNames[x];
+            theadTr.appendChild(theadThEl);
+        }
+
+        // tbody ⇒ tr の数 → オブジェクトの数
+        for(let z = 0; z < objLength; z++){
+            var tbodyTrEl = document.createElement("tr");
+            tbodyTrEl.setAttribute("data-bs-toggle", "modal");
+            tbodyTrEl.setAttribute("data-bs-target", "#exampleModal");
+            const values = Object.values(ary[z]);
+            for(let y = 0; y < keysLength; y++){
+                var tbodyTdEl = document.createElement("td");
+                tbodyTdEl.textContent = values[y];
+                if(y === 0){
+                    tbodyTdEl.setAttribute("scope", "row");
+                }
+                tbodyTrEl.appendChild(tbodyTdEl);
+            }
+            tbody.appendChild(tbodyTrEl);
+        }
     });
 };
 
--- public/js/ui.js
+++ public/js/ui.js
@@ -1,5 +1,41 @@
 import { allReadDB } from "./firebase.js"
 
-window.onload = () =>{
-    allReadDB();
-}
+// ターゲット
+var targetEl = document.getElementById("tableArea");
+
+// table
+var tableEl = document.createElement("table");
+tableEl.setAttribute("class", "table table-hover table-border");
+
+// thead
+var theadEl = document.createElement("thead");
+var theadTrEl = document.createElement("tr");
+
+// tbody
+var tbodyEl = document.createElement("tbody");
+
+// 編集
+allReadDB(theadTrEl, tbodyEl);
+
+// thead結合
+theadEl.appendChild(theadTrEl);
+
+// ボタン作成
+var trBtnEl = document.createElement("tr");
+trBtnEl.setAttribute("class", "table-primary");
+trBtnEl.setAttribute("data-bs-toggle", "modal");
+trBtnEl.setAttribute("data-bs-target", "#exampleModal");
+var tdBtnEl = document.createElement("td");
+tdBtnEl.setAttribute("colspan", "3");
+var btnElText = "追加する";
+tdBtnEl.textContent = btnElText;
+trBtnEl.appendChild(tdBtnEl);
+// タイミングを考えたい。。。
+tbodyEl.appendChild(trBtnEl);
+
+// table結合
+tableEl.appendChild(tbodyEl);
+
+// ターゲット結合
+targetEl.appendChild(tableEl);
+tableEl.appendChild(theadEl);
